d21e90c02d94e53fe6e2fe794836c4a63c64155b
Misss13 <zuzia.konopek@gmail.com>
2022-01-02 19:13:11 +0100

    °˖✧◝(⁰▿⁰)◜✧˖°

--- generator_hasel.py
+++ generator_hasel.py
@@ -1,14 +1,15 @@
-import json
+#import json
 import hashlib
 
 nazwa = input("Podaj nr indexu: ")
 haslo = input("Podaj haslo: ")
 
 haslo = str(hashlib.sha256(haslo.encode()).hexdigest())
+Slownik_hasel = {}
 
-Slownik_hasel = json.load(open("shadow.txt"))
+#Slownik_hasel = json.load(open("shadow.txt"))
 Slownik_hasel[nazwa]=haslo
 print(Slownik_hasel)
-json.dump(Slownik_hasel, open("shadow.txt",'w'))
+#json.dump(Slownik_hasel, open("shadow.txt",'w'))
 
 #, "000001": "a7fda0b61e2047f0f1057d1f5f064c272fd5d490961c531f4df64b0dd354683a"
