9b82a621525fdc4acbe65da35b369b7c4b955f50
Misss13 <zuzia.konopek@gmail.com>
2022-01-10 16:05:56 +0100

    o0O.O0o

--- do_crona_default.sh
+++ do_crona_default.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+screen -X -S serwer_nowy quit && echo "[SUCCESS] closed screen" || echo "[ERROR] can't close"
+
 killall screen
 kill `ps aux | grep serwer_nowy_dzialajacy.py | cut -d" " -f2 | head -n 1`  &>/dev/null || echo "[DOWN] serwer_nowy_default.py"
 kill `ps aux | grep serwer_nowy.py | cut -d" " -f2 | head -n 1` &>/dev/null || echo "[DOWN] serwer_nowy.py"
+
 cd /home/Balalaika/domains/balalaika.ct8.pl/public_html/
-screen -t python-serwer -d -m python serwer_nowy.py ser && echo "[ONLINE] serwer_nowy.py"
+screen -dmS serwer_nowy python serwer_nowy.py lok && echo "[ONLINE] serwer_nowy.py" || echo "[ERROR]"
 echo "ahoj_default" > status
 touch status
