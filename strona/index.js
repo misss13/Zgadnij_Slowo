c80d07f58a8b1ca552369b4c28da8cb184fd7d40
Misss13 <zuzia.konopek@gmail.com>
2022-01-12 23:47:40 +0100

    o

--- strona/index.js
+++ strona/index.js
@@ -1,7 +1,7 @@
 var data 
 $.getJSON('http://balalaika.ct8.pl/punkty.json', function(data) {
       new gridjs.Grid({
-        columns: ["Numer indexu", "Punkty"],
+        columns: ["Numer indexu", "Punkty z 32 gier"],
         sort: true,
         search: true,
         data: data,
@@ -89,4 +89,36 @@ $.getJSON('http://balalaika.ct8.pl/kolejka.json', function(data) {
       }).render(document.getElementById("Kolejka"));
 });
 
+$.getJSON('http://balalaika.ct8.pl/punkty.z30rund.json', function(data) {
+      new gridjs.Grid({
+        columns: ["Numer indexu", "Ilosc punktów w 30 grach"],
+        sort: true,
+        search: true,
+        data: data,
+        style: {
+          table: {
+            border: '3px rgba(153, 142, 142, 0)'
+          },
+          th: {
+            'background-color': 'rgba(224, 80, 80, 0.952)',
+            color: '#fff',
+            'border-bottom': '3px #ccc',
+            'text-align': 'center'
+          },
+          td: {
+            'background-color': 'rgba(53, 51, 51, 1)',
+            color: '#fff',
+            'text-align': 'center'
+          },
+          container : {
+            'background-color': 'rgba(53, 51, 51, 1)',
+            color: '#000',
+          },
+          search: {
+            'background-color': 'rgba(53, 51, 51, 1)',
+            color: '#000',
+          }
+        }
+      }).render(document.getElementById("pomylka"));
+});
 //format na pythona TODO [["NR INDEXU", "PUNKTY"],[kol osoba]....]
